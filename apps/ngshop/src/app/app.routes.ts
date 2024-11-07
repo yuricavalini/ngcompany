d24eefec424272536d9b2701c4f5f7d1ab021776
Yuri Cavalini <yuricavalini@gmail.com>
2024-11-07 08:24:32 -0300

    feat(ngshop): update routes

--- apps/ngshop/src/app/app.routes.ts
+++ apps/ngshop/src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Route } from '@angular/router';
+import { ProductListComponent } from './pages/product-list/product-list.component';
+import { HomeComponent } from './pages/home/home.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: HomeComponent
+  },
+  {
+    path: 'products',
+    component: ProductListComponent,
+  }
+];
