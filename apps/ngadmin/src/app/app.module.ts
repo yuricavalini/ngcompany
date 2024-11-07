fa1875044205168dd7883649aaa99fb0d11d4b0f
Yuri Cavalini <yuricavalini@gmail.com>
2024-11-07 08:03:08 -0300

    feat(ngadmin): add ngadmin app

---
+++ apps/ngadmin/src/app/app.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { AppComponent } from './app.component';
+import { appRoutes } from './app.routes';
+import { NxWelcomeComponent } from './nx-welcome.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+
+@NgModule({
+  declarations: [AppComponent, NxWelcomeComponent, DashboardComponent],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, { initialNavigation: 'enabledBlocking' })
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
