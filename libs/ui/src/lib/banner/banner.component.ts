b0c68a5faa8ae107f1bc9a65874ce5de250650f9
Yuri Cavalini <yuricavalini@gmail.com>
2024-11-08 18:45:23 -0300

    feat(ngcompany): struture style files for applications

--- libs/ui/src/lib/banner/banner.component.ts
+++ libs/ui/src/lib/banner/banner.component.ts
@@ -3,6 +3,5 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'ui-banner',
   templateUrl: './banner.component.html',
-  styleUrls: ['./banner.component.scss']
 })
 export class BannerComponent {}
