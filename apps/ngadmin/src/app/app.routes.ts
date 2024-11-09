ba3621837950ebdeb089d84b0a9a84053ad124e8
Yuri Cavalini <yuricavalini@gmail.com>
2024-11-09 10:56:53 -0300

    feat(ngshop): add basic navigation and style

--- apps/ngadmin/src/app/app.routes.ts
+++ apps/ngadmin/src/app/app.routes.ts
@@ -1,3 +1,16 @@
 import { Route } from '@angular/router';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ShellComponent } from './shared/shell/shell.component';
 
-export const appRoutes: Route[] = [];
+export const appRoutes: Route[] = [
+  {
+    path: '',
+    component: ShellComponent,
+    children: [
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ]
+  }
+];
