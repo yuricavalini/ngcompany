7291b44708722a5db44704be3e2d50a761d28d2c
Yuri Cavalini <yuricavalini@gmail.com>
2024-11-16 11:02:35 -0300

    feat(ngcompany): add new logic for put method and minor changes for post method in InMemoryDataService

--- data/in-memory-data.service.ts
+++ data/in-memory-data.service.ts
@@ -3,6 +3,7 @@ import { InMemoryDbService, RequestInfo } from 'angular-in-memory-web-api';
 
 import { CategoriesFakeDb } from './categories-db-fake';
 import { ProductsFakeDb } from './products-db-fake';
+import { Product } from '../libs/products/src';
 
 type GenericObject<T> = T & { id: string };
 
@@ -28,6 +29,57 @@ export class InMemoryDataService implements InMemoryDbService {
   }
 
   // Optional: Override POST response
+  put(requestInfo: RequestInfo) {
+    const collectionName = requestInfo.collectionName;
+
+    if (collectionName === 'products') {
+      // Extract raw request body
+      const rawBody = requestInfo.utils.getJsonBody(requestInfo.req);
+
+      // Check if the body is FormData and convert it to JSON
+      const jsonBody = this.formDataToJson(rawBody);
+
+      if (!jsonBody) {
+        return requestInfo.utils.createResponse$(() => ({
+          body: { error: 'Invalid form data' },
+          status: 400
+        }));
+      }
+
+      // Update boolean field
+      jsonBody['isFeatured'] = jsonBody['isFeatured'] === 'true';
+
+      // Update number fields
+      jsonBody['price'] = parseFloat(jsonBody['price']);
+      jsonBody['countInStock'] = parseInt(jsonBody['countInStock'], 10);
+
+      // Update category object with the new product
+      const category = CategoriesFakeDb.categories.find(
+        (category) => category.id === jsonBody['category']
+      );
+
+      const updatedData = { ...jsonBody, category };
+
+      // Update the object in the collection
+      const productIndex = requestInfo.collection.findIndex(
+        (item: Product) => item.id === jsonBody['id']
+      );
+      requestInfo.collection[productIndex] = {
+        ...requestInfo.collection[productIndex],
+        ...updatedData
+      };
+
+      // Return the created object in the response
+      return requestInfo.utils.createResponse$(() => ({
+        body: requestInfo.collection[productIndex],
+        status: 200
+      }));
+    }
+
+    return undefined; // Default handling for other collections
+  }
+
+  // Optional: Override POST response
   post(requestInfo: RequestInfo) {
     const collectionName = requestInfo.collectionName;
 
@@ -45,6 +97,15 @@ export class InMemoryDataService implements InMemoryDbService {
         }));
       }
 
+      // Update boolean field
+      jsonBody['isFeatured'] = jsonBody['isFeatured'] === 'true';
+
+      // Update number fields
+      jsonBody['price'] = parseFloat(jsonBody['price']);
+      jsonBody['countInStock'] = parseInt(jsonBody['countInStock'], 10);
+      jsonBody['rating'] = 0;
+      jsonBody['numReviews'] = 0;
+
       // Update category object with the new product
       const category = CategoriesFakeDb.categories.find(
         (category) => category.id === jsonBody['category']
@@ -58,7 +119,6 @@ export class InMemoryDataService implements InMemoryDbService {
 
       const createdObject = { ...jsonBody, dateCreated, category, id };
       requestInfo.collection.push(createdObject);
-
 
       // Return the created object in the response
       return requestInfo.utils.createResponse$(() => ({
