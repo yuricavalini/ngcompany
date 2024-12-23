071dae5fe77d92b54fab2c96d655e452d36f3ebc
Yuri Cavalini <yuricavalini@gmail.com>
2024-12-23 17:33:50 -0300

    feat(ngcompany): add logout feature

--- apps/ngadmin/src/app/shared/sidebar/sidebar.component.ts
+++ apps/ngadmin/src/app/shared/sidebar/sidebar.component.ts
@@ -1,7 +1,15 @@
 import { Component } from '@angular/core';
+import { AuthService } from '@ngcompany/users';
 
 @Component({
   selector: 'ngadmin-sidebar',
   templateUrl: './sidebar.component.html',
 })
-export class SidebarComponent {}
+export class SidebarComponent  {
+
+  constructor(private authService: AuthService) { }
+
+  logoutUser() {
+    this.authService.logout();
+  }
+}
--- libs/users/src/lib/services/auth.service.ts
+++ libs/users/src/lib/services/auth.service.ts
@@ -1,6 +1,9 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Router } from '@angular/router';
+
 import { User } from '../models/user';
+import { JwTService } from './jwt.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +12,14 @@ export class AuthService {
   // apiURLUsers = environment.apiUrl + 'users';
   apiURLUsers = 'api/users';
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private jwtService: JwTService, private router:Router) {}
 
   login(email: string, password: string) {
     return this.http.post<User>(`${this.apiURLUsers}/login`, { email, password });
+  }
+
+  logout() {
+    this.jwtService.removeToken();
+    this.router.navigate(['/login']);
   }
 }
