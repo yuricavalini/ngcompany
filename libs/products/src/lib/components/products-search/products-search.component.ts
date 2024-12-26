2342f101f7d908bbe4a290fc069738a7e4fa734f
Yuri Cavalini <yuricavalini@gmail.com>
2024-12-26 15:42:36 -0300

    feat(ngcompany): add products-search component

---
+++ libs/products/src/lib/components/products-search/products-search.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'ngcompany-products-search',
+  templateUrl: './products-search.component.html',
+  styles: []
+})
+export class ProductsSearchComponent {}
--- libs/products/src/lib/products.module.ts
+++ libs/products/src/lib/products.module.ts
@@ -1,7 +1,11 @@
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+
+import { ProductsSearchComponent } from './components/products-search/products-search.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [ProductsSearchComponent],
+  exports: [ProductsSearchComponent]
 })
 export class ProductsModule {}
