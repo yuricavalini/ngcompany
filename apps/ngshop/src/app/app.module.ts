3063b5ee115e2877bb76c4ec2fc646e12f701969
Yuri Cavalini <yuricavalini@gmail.com>
2025-01-15 20:01:11 -0300

    feat(ngshop): add AuthGuard to checkout page

--- apps/ngshop/src/app/app.module.ts
+++ apps/ngshop/src/app/app.module.ts
@@ -15,6 +15,7 @@ import { HomeComponent } from './pages/home/home.component';
 import { FooterComponent } from './shared/footer/footer.component';
 import { HeaderComponent } from './shared/header/header.component';
 import { NavComponent } from './shared/nav/nav.component';
+import { UsersModule } from '@ngcompany/users';
 
 @NgModule({
   declarations: [
@@ -32,6 +33,7 @@ import { NavComponent } from './shared/nav/nav.component';
     UiModule,
     ProductsModule,
     OrdersModule,
+    UsersModule,
     HttpClientInMemoryWebApiModule.forRoot(InMemoryDataService, {
       dataEncapsulation: false
     }),
--- libs/orders/src/lib/lib.routes.ts
+++ libs/orders/src/lib/lib.routes.ts
@@ -3,6 +3,7 @@ import { Route } from '@angular/router';
 import { CartComponent } from './pages/cart/cart.component';
 import { CheckoutComponent } from './pages/checkout/checkout.component';
 import { ThankYouComponent } from './pages/thank-you/thank-you.component';
+import { AuthGuard } from '@ngcompany/users';
 
 
 export const ordersRoutes: Route[] = [
@@ -12,6 +13,7 @@ export const ordersRoutes: Route[] = [
   },
   {
     path: 'checkout',
+    canActivate: [AuthGuard],
     component: CheckoutComponent
   },
   {
